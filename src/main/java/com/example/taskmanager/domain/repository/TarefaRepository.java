a2da62c0dd98225a65045a3ce4ded299ee134b11
IsraelHF <israel.com.ihf80@gmail.com>
2023-09-05 21:34:23 -0300

    Adições pontuais

---
+++ src/main/java/com/example/taskmanager/domain/repository/TarefaRepository.java
@@ -0,0 +1,5 @@
+package com.example.taskmanager.domain.repository;
+
+public class TarefaRepository {
+    
+}
---
+++ src/main/java/com/example/taskmanager/domain/service/ListaDeTarefasService.java
@@ -0,0 +1,5 @@
+package com.example.taskmanager.domain.service;
+
+public class ListaDeTarefasService {
+    
+}
